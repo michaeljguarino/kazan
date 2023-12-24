ef2050c547ab74c283ef02397925d48637bd67a1
michaeljguarino <mguarino46@gmail.com>
2023-12-24 13:32:05 -0500

    fix number parsing

--- lib/kazan/models.ex
+++ lib/kazan/models.ex
@@ -116,6 +116,7 @@ defmodule Kazan.Models do
   defp decode_property(value, %{type: :string}), do: {:ok, value}
   defp decode_property(value, %{type: :boolean}), do: {:ok, value}
   defp decode_property(value, %{type: :integer}), do: {:ok, value}
+  defp decode_property(value, %{type: :number}), do: {:ok, value}
   defp decode_property(value, %{type: :object}), do: {:ok, value}
 
   defp decode_property(value, %{type: :array, items: items}) do
@@ -173,6 +174,7 @@ defmodule Kazan.Models do
   defp encode_property(value, %{type: :string}), do: {:ok, value}
   defp encode_property(value, %{type: :boolean}), do: {:ok, value}
   defp encode_property(value, %{type: :integer}), do: {:ok, value}
+  defp encode_property(value, %{type: :number}), do: {:ok, value}
   defp encode_property(value, %{type: :object}), do: {:ok, value}
 
   defp encode_property(value, %{type: {:array, items}}) when is_atom(items),
